1c10a469d745b3e2b912763d232d045f50a25f3c
kastori <130933131+FrancescoStori@users.noreply.github.com>
2024-10-23 16:43:04 +0200

    toylexer task 1 completata

--- toylexer/lib/main.ml
+++ toylexer/lib/main.ml
@@ -23,5 +23,25 @@ let string_of_tokenlist tl =
 let string_of_frequencies fl =
   List.fold_left (fun s (t,n) -> s ^ ((string_of_token t) ^ " -> " ^ string_of_int n ^ "\n")) "" fl
 
+
+
+(*let uniq l = List.sort_uniq (fun e e' -> if e = e' then 0 else -1 ) l;;*)
+
+let uniq l = List.fold_left 
+  (fun l' e -> if (List.for_all (fun e' -> e' <> e) l') then e::l' else l') [] l;;
+
+let get_n n l = 
+  List.fold_left (fun l' e -> if (List.length l' < n) then e::l' else l') [] l
+  |> List.rev
+;;
+
+let count_occ e l = (List.filter (fun e' -> e' = e) l) |> List.length;;
+
 (* frequency : int -> 'a list -> ('a * int) list *)
-let frequency _ _ = failwith("TODO")
+
+let frequency n (l) =
+  let ul = uniq l in
+  List.fold_left (fun l' e_ul -> (e_ul, count_occ e_ul l)::l') [] ul
+  |> List.sort (fun (_, x) (_, y) -> if x=y then 0 else if x>y then -1 else 1)
+  |> get_n n
+;;
